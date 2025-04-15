e8f1077dc8ae1731d3dcbeb6b14ffebfdd97f26b
Divyansh0702 <divyansh07020@gmail.com>
2025-04-15 23:32:00 +0530

    Array

---
+++ src/Assignment_3/Maximum_Score.java
@@ -0,0 +1,42 @@
+package Assignment_3;
+
+import java.util.Scanner;
+
+public class Maximum_Score {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-- > 0) {
+			int n = sc.nextInt();
+			int m = sc.nextInt();
+			
+			int nums1[] = new int[n];
+			for (int i = 0; i < nums1.length; i++) {
+				nums1[i] = sc.nextInt();
+			}
+			
+			int nums2[] = new int[m];
+			for (int i = 0; i < nums2.length; i++) {
+				nums2[i] = sc.nextInt();
+			}
+			
+			
+		}
+	}
+	
+	public static void MaxScore(int[] nums1, int[] nums2  ) {
+		
+	}
+
+}
+
+
+
+
+
+
+
+
+
