a0f1b72960409c9c434c5e412909b7862699ed54
Divyansh0702 <divyansh07020@gmail.com>
2025-04-18 23:51:56 +0530

    Array

--- src/Assignment_3/Maximum_Circular_Sum.java
+++ src/Assignment_3/Maximum_Circular_Sum.java
@@ -10,17 +10,32 @@ public class Maximum_Circular_Sum {
 	}
 	
 	public static int maxCircSum(int[] nums) {
-		int totalsum = 0, minsum = 0, minkadane = Integer.MAX_VALUE;
-		
+		int linear_sum = Kadanes(nums);
+		int sum = 0;
 		for (int i = 0; i < nums.length; i++) {
-			totalsum += nums[i];
-			
-			minsum += nums[i];
-			minkadane = Math.min(minsum, minkadane);
-			
+			sum += nums[i];
+			nums[i] = nums[i] * -1;
+		}
+		int mid = Kadanes(nums);
+		int circular_sum = sum + mid;
+		
+		if(circular_sum == 0) {
+			return linear_sum;
 		}
 		
-		return totalsum - minkadane;
+		return Math.max(linear_sum, circular_sum);
+	}
+	
+	public static int Kadanes(int[] nums) {
+		int sum = 0, ans = Integer.MIN_VALUE;
+		for (int i = 0; i < nums.length; i++) {
+			sum += nums[i];
+			ans = Math.max(ans, sum);
+			if(sum < 0) {
+				sum = 0;
+			}
+		}
+		return ans;
 	}
 
 }
