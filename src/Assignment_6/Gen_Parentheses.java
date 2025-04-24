d49a81f74ba9b91682548e7c527d3fafac1762c9
Divyansh0702 <divyansh07020@gmail.com>
2025-04-24 19:19:44 +0530

    Recursion

---
+++ src/Assignment_6/Gen_Parentheses.java
@@ -0,0 +1,48 @@
+package Assignment_6;
+
+public class Gen_Parentheses {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 3;
+		int open = 0, close = 0;
+		
+		
+	}
+	
+//	public static void Generate(int open, int close, String ans) {
+//		
+//		Generate(open, close, );
+//	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/Assignment_6/Maze_Path.java
+++ src/Assignment_6/Maze_Path.java
@@ -1,10 +1,45 @@
 package Assignment_6;
 
+import java.util.Scanner;
+
 public class Maze_Path {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		int[][] maze = new int[n][m];
+		
+		Path(m, n, 0, 0, "");
+		System.out.println();
+		System.out.println(CountPath(m, n, 0, 0, ""));
+	}
+	
+	public static void Path(int m, int n, int CR, int CC, String ans) {
+		if(CR > n-1 || CC > m-1) {
+			return;
+		}
+		if(CR == n-1 && CC == m-1) {
+			System.out.print(ans + " ");
+			return;
+		}
+		Path(m, n, CR + 1, CC, ans + "V");
+		Path(m, n, CR, CC + 1, ans + "H");
+		Path(m, n, CR + 1, CC + 1, ans + "D");
+	}
+	
+	public static int CountPath(int m, int n, int CR, int CC, String ans) {
+		if(CR > n-1 || CC > m-1) {
+			return 0;
+		}
+		if(CR == n-1 && CC == m-1) {
+			return 1;
+		}
+		int a1 = CountPath(m, n, CR, CC + 1, ans + "H");
+		int a2 = CountPath(m, n, CR + 1, CC, ans + "V");
+		int a3 = CountPath(m, n, CR + 1, CC + 1, ans + "D");
+		return a1 + a2 + a3;
 	}
 
 }
