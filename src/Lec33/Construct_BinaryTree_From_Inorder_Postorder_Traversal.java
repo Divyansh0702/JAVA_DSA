b1c8ea6d42d11a450fd3f0362a1cd61ace5f9e90
Divyansh0702 <divyansh07020@gmail.com>
2025-09-09 21:44:42 +0530

    BST

--- src/Lec33/Construct_BinaryTree_From_Inorder_Postorder_Traversal.java
+++ src/Lec33/Construct_BinaryTree_From_Inorder_Postorder_Traversal.java
@@ -1,6 +1,6 @@
 package Lec33;
 
-public class Construct_BinaryTree_From_Preorder_Inorder_Traversal {
+public class Construct_BinaryTree_From_Inorder_Postorder_Traversal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
