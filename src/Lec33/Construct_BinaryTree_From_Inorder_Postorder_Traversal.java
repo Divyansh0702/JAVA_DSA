06facc841db43b536fddf86a8535640d6993468f
Divyansh0702 <divyansh07020@gmail.com>
2025-09-11 22:55:13 +0530

    BST

--- src/Lec33/Construct_BinaryTree_From_Inorder_Postorder_Traversal.java
+++ src/Lec33/Construct_BinaryTree_From_Inorder_Postorder_Traversal.java
@@ -4,7 +4,7 @@ public class Construct_BinaryTree_From_Inorder_Postorder_Traversal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		// Go find at leetcode
+		// Go find at leetcode 
 	}
 
 }
