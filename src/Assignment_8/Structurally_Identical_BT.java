a2869b1688994c4c8682a1ab54dd770886a40afe
Divyansh0702 <divyansh07020@gmail.com>
2025-09-20 23:40:20 +0530

    Tree & BST

--- src/Assignment_8/Structurally_Identical_BT.java
+++ src/Assignment_8/Structurally_Identical_BT.java
@@ -68,7 +68,8 @@ public class Structurally_Identical_BT {
 		BinaryTree bt2 = SIBT.new BinaryTree();
 		
 		System.out.println(bt1.StructurallyIdentical(bt2));
-		
+
+//		Input
 //		10 true 20 true 40 false false true 50 false false true 30 true 60 false false true 73 false false
 //		10 true 20 true 40 false false true 50 false false true 30 true 60 false false true 73 false false
 	}
