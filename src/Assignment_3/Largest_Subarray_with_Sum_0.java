b093ca2a0de377f9e6921a48a3d97f159b0ddc93
Divyansh0702 <divyansh07020@gmail.com>
2026-02-22 01:10:27 +0530

    Array

--- src/Assignment_3/Largest_Subarray_with_Sum_0.java
+++ src/Assignment_3/Largest_Subarray_with_Sum_0.java
@@ -1,11 +1,14 @@
 package Assignment_3;
 
+import java.util.*;
+
 public class Largest_Subarray_with_Sum_0 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[] nums = {9, -3, 3, -1, 6, -5} ;
 		System.out.println(LargestSubarray(nums));
+		System.out.println(LargestSubarray2(nums));
 	}
 	
 	public static int LargestSubarray(int[] nums) {
@@ -21,6 +24,28 @@ public class Largest_Subarray_with_Sum_0 {
 			}
 		}
 		return res;
+	}
+	
+	public static int LargestSubarray2(int[] nums) {
+		int len = 0, prefixSum = 0;
+		
+		HashMap<Integer, Integer> map = new HashMap<>();
+		
+		for(int i = 0; i < nums.length; i++) {
+			prefixSum += nums[i];
+			
+			if(prefixSum == 0) {
+				len = Math.max(len, i + 1);
+			}
+			else if(map.containsKey(prefixSum)) {
+				len = Math.max(len, i - map.get(prefixSum));
+			}
+			else {
+				map.put(prefixSum, i);
+			}
+		}
+		
+		return len;
 	}
 
 }
